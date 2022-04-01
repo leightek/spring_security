68378c38453761695336f293f59c2791ea1fbdfa
leightek <leighsuconsulting@gmail.com>
2022-04-01 00:00:20 -0700

    [SpringSecurity] Initialized repository

---
+++ src/test/java/com/leightek/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leightek.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
